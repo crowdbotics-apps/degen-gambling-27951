f8dffc0f8c3bbbc6cbdc099f18add24d790737e1
Crowdbotics <team@crowdbotics.com>
2021-06-13 16:37:26 +0000

    Initial commit.

---
+++ degen_gambling_27951/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for degen_gambling_27951 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'degen_gambling_27951.settings')
+
+application = get_wsgi_application()
